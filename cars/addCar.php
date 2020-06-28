6ec345c2eddf03ac86bf5068cab0ebcbf65d8180
Samuel Bouye <bouyesamuel@gmail.com>
2020-06-28 01:10:22 +0000

    add new car in database

--- cars/addCar.php
+++ cars/addCar.php
@@ -6,17 +6,17 @@ require_once("../header_navbar.php");
 ?>
 <br>
 <div class="container">
-    <form action="newCar.php">
-        <div class="form-groups">
-            <label for="mark">Mark</label>
-            <input type="text" id="mark" class="form-control" name="mark" placeholder="BMW">
-        </div>
-        <br>
-        <div class="form-groups">
-            <label for="Model">Model</label>
-            <input type="text" id="model" class="form-control" name="model" placeholder="X6">
-        </div>
-        <br>
+    <form action="/TakeMyCar/db/connection.php" method="post">
+    <br>
+    <div class="form-groups">
+        <label for="Model">Model</label>
+        <input type="text" id="model" class="form-control" name="model" placeholder="X6">
+    </div>
+    <br>
+    <div class="form-groups">
+        <label for="brand">Brand</label>
+        <input type="text" id="brand" class="form-control" name="brand" placeholder="BMW">
+    </div>
         <div class="form-groups">
             <label for="description">Description</label>
             <textarea name="description" class="form-control" id="description" cols="30" rows="10" placeholder="Description of your car"></textarea>
--- db/connection.php
+++ db/connection.php
@@ -1,11 +1,26 @@
 <!-- Connection to database -->
 <?php 
 $mysqli = new mysqli("localhost", "root", "root", "takemycardb");
-if($mysqli->errno)
+if(mysqli_connect_errno())
 {
-    echo "Error connection database " . $mysqli->errno;
+    echo "Error connection database " . $mysqli->connect_error;
 }
 else
 {
     echo 'Connection successful';
 }
+// $sql = "INSERT INTO cars(model,brand,description) VALUES ('".$POST['model']."','".$POST['brand'] ."','".$POST['description']."')";
+$sql = "INSERT INTO cars(model,brand,description) VALUES ('".$_POST['model']."','".$_POST['brand'] ."','".$_POST['description']."')";
+
+
+if(mysqli_query($mysqli, $sql))
+{
+    echo 'Adding successful\n';
+    var_dump($_POST);
+}
+else
+{
+    echo "Error " . $sql . "" . mysqli_error($mysqli);
+}
+
+$mysqli->close();
