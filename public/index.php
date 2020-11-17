e87a5dae68b7de6e3808940f7b0fda9168083f8a
Samuel Bouye <bouyesamuel@gmail.com>
2020-11-17 23:47:23 +0000

    change project struture

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+require '../vendor/autoload.php';
+
+require_once('../header.php');
+require_once('../header_navbar.php');
+?>
+
+<main role="main">
+<?php     
+    require('../intro_section.php');
+    require('../list_cars.php');
+?>
+</main>
+
+<?php 
+require('../footer.php');
+require('../script_js.php');
+?>
