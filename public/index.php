675fb8fd21680629fe15826a1de28b99a5beda87
Samuel Bouye <bouyesamuel@gmail.com>
2020-11-18 00:56:42 +0000

    add some error extension , var_dumper extension and altrouter for route and add configuration of altrouter

--- public/index.php
+++ public/index.php
@@ -1,18 +1,26 @@
 <?php
 require '../vendor/autoload.php';
+$router = new AltoRouter();
 
-require_once('../header.php');
-require_once('../header_navbar.php');
-?>
 
-<main role="main">
-<?php     
-    require('../intro_section.php');
-    require('../list_cars.php');
-?>
-</main>
+$router->map('GET','/', function(){
+    require '../src/views/home.php';
+}, 'home');
 
-<?php 
-require('../footer.php');
-require('../script_js.php');
-?>
+$router->map('GET','/about', function(){
+    // require __DIR__ . '../views/home.php';
+     echo "about page";
+}, 'about');
+
+
+$match = $router->match();
+
+// dump($match);
+
+
+if ($match && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+}else {
+	// no route was matched
+    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
---
+++ src/views/home.php
@@ -0,0 +1,5 @@
+<?php require_once __DIR__ . "/layouts/layout.php";
+
+$content = "Hello from home page";
+
+?>
